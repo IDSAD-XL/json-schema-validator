755233957254ac55d06dd8bc5bdaf290ec65b431
IDSAD <idsad.ch@gmail.com>
2023-01-10 17:46:18 +0200

    backend (3)

---
+++ api/_helpers/errorTypes.js
@@ -0,0 +1,4 @@
+export const errorTypes = {
+  EMAIL_IS_TAKEN: 'This email is already taken',
+  AUTH_ERROR: "Authentication failed"
+}
--- api/users/users.service.js
+++ api/users/users.service.js
@@ -1,9 +1,40 @@
 const db = require('../_helpers/db')
+const { errorTypes } = require("../_helpers/errorTypes");
+const jwt = require("../_helpers/jwt");
 
 const User = db.User
 
 async function create(payload) {
   if (await User.findOne({ email: payload.email })) {
-    return {  }
+    return { error: errorTypes.EMAIL_IS_TAKEN }
+  }
+
+  const user = new User(payload)
+
+  if (payload.password) {
+    user.hash = bcrypt.hashSync(payload.password, 10)
+  }
+
+  await user.save()
+
+  // const authUser =
+}
+
+async function authenticate({ email, username, password }) {
+  const user = await User.findOne({ email })
+
+  if (user && bcrypt.compareSync(password, user.hash)) {
+    const token = jwt.sign({ sub: user.id }, process.env.JWT_SECRET, {
+      expiresIn: "7d"
+    })
+
+    return {
+      ...user.toJSON(),
+      token
+    }
+  }
+
+  return {
+    error: errorTypes.AUTH_ERROR
   }
 }
